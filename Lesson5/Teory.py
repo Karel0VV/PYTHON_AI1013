e49e8a9341942551f91b2ac2ee569cb9df6ca7a3
Jeka <jekakrv209@gmail.com>
2023-11-09 22:41:47 +0200

    My first commit

--- Lesson5/Teory.py
+++ Lesson5/Teory.py
@@ -2,10 +2,12 @@ from colorama import Fore, Back, Style
 # Fore - задає колір тексту в консолі
 print(Fore.RED + "text")
 print("some text")
-#
+# Back - задає задній кольір
 print(Back.GREEN + "Green")
 print("some text")
 print(Fore.BLUE + "BLUE text")
+# Style.DIM
 print(Style.DIM +"Dim text")
+# Style.RESET_ALL - задає всі налаштування
 print(Style.RESENT_ALL)
 print("some text")
