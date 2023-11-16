ce0d6d9d87f3d36968017e073c2b24e2964494d7
Jeka <jekakrv209@gmail.com>
2023-11-16 23:00:46 +0200

    My first commit

--- Dz6.py
+++ Dz6.py
@@ -5,8 +5,19 @@ def divider(a, b):
     if b > 100:
         raise IndexError
     return a/b
+# data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8 : 4}
 data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8 : 4}
+
 for key in data:
-    res = divider(key, data[kem])
-result.append(res)
+    try:
+        res = divider(key, data[kem])
+        result.append(res)
+    except TypeError:
+        print("TypeError")
+    except ValueError:
+        print("ValueError")
+    except ZeroDivisionError:
+        print("ZeroDivisionError")
+
+
 print(result)
